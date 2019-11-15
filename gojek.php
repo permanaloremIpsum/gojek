45294cf254fc23635e599216c36a3579da1c1327
permanaloremIpsum <dpermana350@gmail.com>
2019-11-15 08:49:11 +0700

    first commit

---
+++ gojek.php
@@ -0,0 +1,57 @@
+<?php
+error_reporting(0);
+if (!file_exists('token')) {
+    mkdir('token', 0777, true);
+}
+include ("curl.php");
+echo "\n";
+echo "\e[94m          Voucher Claim Gojek           \n";
+echo "\e[91m FORMAT NOMOR HP : INDONESIA '62***' , US='1***'\n";
+echo "\e[93m SCRIPT GOJEK AUTO REGISTER + AUTO CLAIM VOUCHER\n";
+echo "\e[93m SCRIPT : Kumpulanremaja.com\n";
+echo "\e[93m Github: 4kumpulanremaja \n";
+echo "\n";
+echo "\e[96m[?] Masukkan Nomor HP Anda (62/1) : ";
+$nope = trim(fgets(STDIN));
+$register = register($nope);
+if ($register == false)
+    {
+    echo "\e[91m[x] Nomor Telah Terdaftar\n";
+    }
+  else
+    {
+    otp:
+    echo "\e[96m[!] Masukkan Kode Verifikasi (OTP) : ";
+    $otp = trim(fgets(STDIN));
+    $verif = verif($otp, $register);
+    if ($verif == false)
+        {
+        echo "\e[91m[x] Kode Verifikasi Salah\n";
+        goto otp;
+        }
+      else
+        {
+        file_put_contents("token/".$verif['data']['customer']['name'].".txt", $verif['data']['access_token']);
+        echo "\e[93m[!] Trying to redeem Voucher : GOFOODSANTAI19 !\n";
+        sleep(3);
+        $claim = claim($verif);
+        if ($claim == false)
+        {
+        echo "\e[92m[!]".$voucher."\n";
+        sleep(3);
+        echo "\e[93m[!] Trying to redeem Voucher : GOFOODPRAKTIS11 !\n";
+        sleep(3);
+        goto pengen;
+        }
+        pengen:
+        $claim = cekvocer($verif);
+        if ($claim == false ) {
+            echo "\033VOUCHER INVALID/GAGAL REDEEM\n";
+        }
+        else{
+            echo "\e[92m[+] ".$claim."\n";
+                
+        }
+    }
+    }
+?>
